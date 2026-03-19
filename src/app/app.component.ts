1510710c265709857cd5951a1a235376ac1fe55e
Nikola Novak <novak.nikola@proton.me>
2026-03-19 01:53:12 +0100

    zatvori meni kad se klikne izvan njega

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
-    standalone: false
+    standalone: false,
+    host: { '(document:click)': 'onClick($event)' }
 })
 export class AppComponent {
+  @ViewChild('dropdownMenu') dropdownMenu!: ElementRef;
   title = 'dnh-web';
   today: Date = new Date();
   protected menuState: 'open' | 'closed' = 'closed';
@@ -15,5 +17,14 @@ export class AppComponent {
   toggle() {
     if (this.menuState == 'open') this.menuState = 'closed';
     else this.menuState = 'open';
+  }
+
+  navigate(where: string) {
+    console.log(where);
+  }
+
+  onClick(event: Event) {
+    if (!this.dropdownMenu.nativeElement.contains(event.target))
+      this.menuState = 'closed';
   }
 }
